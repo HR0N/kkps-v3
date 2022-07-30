97206180d35883ec4a52e4b72e6250c55fc170ca
HR0N <vivante.des@gmail.com>
2022-07-30 17:02:57 +0300

    init

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+include('tg-bot.php');
---
+++ tg-bot.php
@@ -0,0 +1,29 @@
+<?php
+include('vendor/autoload.php');
+use Telegram\Bot\Api;
+include('env.php');
+use env\Env as env;
+
+$telegram = new Api(env::$TELEGRAM_BOT_TOKEN);
+$result = $telegram->getWebhookUpdates();
+
+$text = strtolower($result['message']['text']);
+$chat_id = $result['message']['chat']['id'];
+$name = $result['message']['from']['username'];
+$first_name = $result['message']['from']['first_name'];
+$last_name = $result['message']['from']['last_name'];
+
+
+if($text == '/start'){
+    $reply = "Hello world!";
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+}
+if($text == 'test'){
+    $reply = "Fuck off bitch!";
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+}
+
+// composer require irazasyed/telegram-bot-sdk ^2.0
+//$ composer require vlucas/phpdotenv
+//https://api.telegram.org/bot5591524736:AAGXk3kxgnGrjpIeMvhMM_toBda5NQVTLnQ/setWebHook?url=
+//https://kabanchik-bot.evilcode.space/tg-bot.php
